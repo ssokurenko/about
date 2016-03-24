03d7df03c1f1e65b76139f4b9b28d693c2da23fb
Sergey Sokurenko <s.sokurenko@gmail.com>
2016-03-24 17:09:20 +0100

    Implemented loading spinner and error handling.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,13 @@
 var app = angular.module('githubCard', [])
     .controller('CardController', function($scope, $http){
-        // Default avatar picture
-        // this.avatar_url = "./img/loader.gif";
 
         // Requesting data using ajax
         $http.get("https://api.github.com/users/" + config.githubID)
             .then(function successCallback(response) {
                 $scope.card = response.data;
                 $scope.card.success = true;
-            }, function errorCallback(response) {
-                alert('does not work');
+            }, function errorCallback() {
+                $scope.card.error = true;
             });
+
     });
