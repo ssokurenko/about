086841e5f11707865a1d9eb796b9ca7b58fa762d
Sergey Sokurenko <s.sokurenko@gmail.com>
2016-03-25 09:30:19 +0100

    Fixed blog URL handling.

--- js/app.js
+++ js/app.js
@@ -2,10 +2,15 @@ var app = angular.module('githubCard', [])
     .controller('CardController', function($scope, $http){
 
         // Requesting data using ajax
-        $http.get("https://api.github.com/users/" + config.githubID)
+        $http.get('https://api.github.com/users/' + config.githubID)
             .then(function successCallback(response) {
                 $scope.card = response.data;
                 $scope.card.success = true;
+
+                // Fixing blog URL if the protocol value is not mentioned
+                if ($scope.card.blog.indexOf('http://') < 0) {
+                    $scope.card.blog = 'http://' + $scope.card.blog;
+                }
             }, function errorCallback() {
                 $scope.card.error = true;
             });
